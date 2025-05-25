a7a6d66184b11d57980d2ceb063cfb5efab40d7c
saniqP <undertalesyt@gmail.com>
2025-05-25 15:23:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Находим все ссылки в блоке dowloundBlock
   const downloadLinks = document.querySelectorAll('.dowloundBlock a');
+  const aboutMe = document.querySelector('.aboutMe'); // Используем querySelector для одного элемента
+  const infoIcon = document.getElementById('infoIcon');
+  const languageMessage = document.getElementById('languageMessage');
   
-  // Перебираем все ссылки
+  // Обработчики для ссылок скачивания
   downloadLinks.forEach(link => {
-    link.addEventListener('click', function(e) {
-      // Проверяем, содержит ли href ссылки 'pyPack.zip'
-      if (this.getAttribute('href') === 'pyPack.zip') {
-        // Отменяем стандартное поведение (чтобы сначала показать сообщение)
-        e.preventDefault();
-        
-        // Показываем сообщение
-        const alertMessage = document.getElementById('alertMessage');
-        alertMessage.textContent = 'Внимание! это пак функций которые подключают к main.cpp файлу.';
-        alertMessage.style.display = 'block';
-        
-        // Через 5 секунд скрываем сообщение
-        setTimeout(() => {
-          alertMessage.style.display = 'none';
-        }, 3000);
-        
-        // Продолжаем скачивание после показа сообщения
-        setTimeout(() => {
-          window.location.href = this.getAttribute('href');
-        }, 1000);
+    link.addEventListener('mouseover', function() {
+      if (this.classList.contains('py')) {
+        languageMessage.textContent = 'python';
+        languageMessage.style.backgroundColor = '#b3a920';
+        languageMessage.classList.add('show');
+      }
+      else if (this.classList.contains('cxx')) {
+        languageMessage.textContent = 'c++';
+        languageMessage.style.backgroundColor = '#4ca9d4';
+        languageMessage.classList.add('show');
+      }
+      else if (this.classList.contains('jva')) {
+        languageMessage.textContent = 'java';
+        languageMessage.style.backgroundColor = '#d96e26';
+        languageMessage.classList.add('show');
       }
     });
+    
+    link.addEventListener('mouseout', function() {
+      languageMessage.classList.remove('show');
+    });
+  });
+
+  // Обработчики для infoIcon
+  infoIcon.addEventListener('mouseover', function() {
+    aboutMe.classList.add('show'); // Добавляем класс show
+  });
+  
+  infoIcon.addEventListener('mouseout', function() {
+    aboutMe.classList.remove('show'); // Удаляем класс show
   });
 });
